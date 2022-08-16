f7014b66b1f20dffaf65c173c8f1a4fe548585a7
Richard4493 <63744968+Richard4493@users.noreply.github.com>
2022-08-16 16:43:38 +0530

    Added scroll to top button function

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Function to check if the user types a valid number in the field//
 function mobcheck(){
     var m=document.forms["details"]["mob"].value
     if(isNaN(m))
@@ -6,6 +7,7 @@ function mobcheck(){
         document.getElementById("mob").value="**Enter a valid phone number**"
     }
 }
+//Function to check whether the user inputs invalid mail format in the field//
 function ValidateEmail(inputText)
 {
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -19,3 +21,7 @@ else
   document.getElementById("mail").value="**Enter a valid mail address**"
 }
 }
+function gotoTop() { //function to scroll up to top of page//
+  document.body.scrollTop = 0; //for safari browser//
+  document.documentElement.scrollTop = 0; //for chrome and firefox//
+} 
